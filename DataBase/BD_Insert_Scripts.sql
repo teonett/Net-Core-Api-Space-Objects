b63396dcf01c0a3b8253746fe7d126a56313b086
Jose Teotonio <teo.nett@icloud.com>
2020-05-07 00:07:16 -0300

    Criacao inicial do script de insert inicial

---
+++ DataBase/BD_Insert_Scripts.sql
@@ -0,0 +1,12 @@
+﻿
+-- Planet
+
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Mercury',4879,'Ê38% the size of Earth','0.39 (57.9 million)','3.3022 x 1023', 0);
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Mars',6779,'Ê53% the size of Earth','1.524 (227.9 million)','6.4185 x 1023', 0);
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Venus',12104,'Ê95% the size of Earth','0.723 (108.2 million)','4.8685 x 1024', 0);
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Earth',12756,'Ê100% the size of Earth','1 (149.6 million)','5.9736 x 1024', 1);
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Neptune',49528,'Ê388% the size of Earth','30.06 (4,497.1 million)','10.243 x 1025', 14);
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Uranus',51118,'Ê400% the size of Earth','19.18 (2,871 million)','8.6810 x 1025', 27);
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Saturn',120660,'Ê945% the size of Earth','9.539 (1,427.0 million)','5.6846 x 1026', 62);
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Jupiter',142800,'Ê1120% the size of Earth','5.203 (778.3 million)','1.8986 x 1027', 67);
+insert into planet(name, diameterkm, sizerelative, sundistance, masskg,moons) values('Pluto(Dwarf-Planet',2374,'18% the size of Earth','5906.4 (149.6 million)','1.325 x 1022', 5);
